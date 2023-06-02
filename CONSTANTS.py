2b9cec94534cc127124114bf2cc3e66a1360d330
Manoj C S <91310299+manoj-c-s@users.noreply.github.com>
2023-06-02 20:03:20 +0530

    Update CONSTANTS.py

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,5 +1,5 @@
-EMAIL_ID = "praneshks@gmail.com"   # Put your email id within quotes
-PASSWORD = "qwerty"   # Put your password within quotes
+EMAIL_ID = "manojcs123456@gmail.com"   # Put your email id within quotes
+PASSWORD = "123456"   # Put your password within quotes
 LANGUAGE = "en"     
 
 
